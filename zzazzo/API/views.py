84241c298bccaa29b1f0c0c5a713cd4c97178139
AbuSohan <sohan@inflexionpointbd.com>
2022-12-12 00:19:28 +0600

    Final commit.

--- zzazzo/API/views.py
+++ zzazzo/API/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .serializers import paymentSerializers, userDetailSerializers
 from rest_framework.views import APIView
 from oscar.core.loading import get_model
+from rest_framework.permissions import IsAuthenticated
 
 Payment = get_model('task', 'Payment')
 User = get_model('task', 'User')
@@ -25,5 +25,6 @@ class paymentDetailApiView(generics.RetrieveAPIView):
 
 
 class userDetailApiView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = User.objects.all()
     serializer_class = userDetailSerializers
