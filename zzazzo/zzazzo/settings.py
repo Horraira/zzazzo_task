f6b062e0a3a29c99f2e60521b6175fe9f2433966
AbuSohan <sohan@inflexionpointbd.com>
2022-12-11 14:15:48 +0600

    PostgreSQL db connection setup

--- zzazzo/zzazzo/settings.py
+++ zzazzo/zzazzo/settings.py
@@ -115,11 +115,22 @@ WSGI_APPLICATION = 'zzazzo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'zzazzo',
+       'USER': 'postgres',
+       'PASSWORD': '1234',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
